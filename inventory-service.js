e80f7a3434812854740fd491f3c0fbe24f5e4c06
noredine ban <lhrinourdine9@gmail.com>
2025-03-17 10:43:46 +0000

    third commit

--- inventory-service.js
+++ inventory-service.js
@@ -16,6 +16,9 @@ app.get('/health', (req, res) => res.status(200).send('OK'));
 
 
 
+
+
+
 // Unified inventory update endpoint
 app.post('/inventory/update', (req, res) => {
     const { itemId, quantity } = req.body;
